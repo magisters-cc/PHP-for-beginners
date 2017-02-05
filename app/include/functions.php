3bd6ae27dfcd5536dd76f8f153fd3b85c7a411b3
Vladislav Gritsenko <webmagisters@gmail.com>
2017-02-05 17:33:25 +0400

    #16 Создание страницы поста

--- app/include/functions.php
+++ app/include/functions.php
@@ -28,3 +28,15 @@ function get_posts() {
     
 }
 
+function get_post_by_id($post_id) {
+    global $link;
+    
+    $sql = "SELECT * FROM posts WHERE id = ".$post_id;
+    
+    $result = mysqli_query($link, $sql);
+    
+    $post = mysqli_fetch_assoc($result);
+    
+    return $post;
+}
+
--- post.php
+++ post.php
@@ -1,6 +1,44 @@
 <?php
-echo '<pre>';
+
+ini_set('error_reporting', 'E_ALL');
+ini_set('display_errors', 'E_ALL');
+ini_set('display_startup_errors', 'E_ALL');
 
 $post_id = $_GET['post_id'];
 
-$_POST;
+//ЕСЛИ В POST_ID НЕ ЧИСЛА, ОСТАНОВИМ СКРИПТ
+if (!is_numeric($post_id)) exit();
+
+require_once 'app/header.php';
+
+//ПОЛУЧАЕМ МАССИВ ПОСТА
+$post = get_post_by_id($post_id);
+?>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-9">
+            <div class="page-header">
+                <h1><?=$post['title']?></h1>
+            </div>
+            <ul class="list-inline">
+                <li><i class="glyphicon glyphicon-list"></i> <a href="#">Название категории</a> | </li>
+                <li><i class="glyphicon glyphicon-calendar"></i> 14 июля 2016 21:00
+            </ul>
+            <hr>
+            <div class="post-content">
+                <img src="<?=$post['image']?>" align="left" style="padding: 0 10px 10px 0;">
+                <?=$post['content']?>
+            </div>
+        </div>
+        <div class="col-md-3">
+            sidebar
+        </div>
+    </div>
+</div>
+
+
+<?php
+    include_once 'app/footer.php';
+?>
